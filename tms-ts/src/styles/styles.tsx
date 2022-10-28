623fb7ff0d62f65ecb8137778c935f7417264545
Daniel <danielsheh02@gmail.com>
2022-10-28 13:23:07 +0300

    Implemented creation pages of case and suite.

--- tms-ts/src/styles/styles.tsx
+++ tms-ts/src/styles/styles.tsx
@@ -9,5 +9,41 @@ export default makeStyles({
     paperCreationTestCase: {
         minWidth: "94%",
         minHeight: "93%",
+    },
+    paperCreationSuite: {
+        minWidth: "55%",
+        minHeight: "55%",
+    },
+    dialogTitle: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginTop: -20
+    },
+    textFieldCreationCase: {
+        maxWidth: "90%",
+    },
+    gridContent: {
+        marginTop: 20,
+    },
+    stackTags: {
+        maxWidth: "90%",
+        maxHeight: 150,
+        marginTop: 8,
+        overflowY: "auto",
+    },
+    tagsInput: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginTop: -20
+    },
+    tut: {
+        backgroundColor: "red"
+    },
+    buttonTxt: {
+        "& .MuiButton-textSizeMedium": {
+            fontSize: 50
+        },
     }
 });
